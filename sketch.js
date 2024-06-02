5f81a3af0912c8c67df9534ee1a75042fde0bfb4
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-06-02 15:52:23 +0530

    Adde More Detail of Regular Expression

--- sketch.js
+++ sketch.js
@@ -11,9 +11,27 @@ function setup(){
     submit.mousePressed(newText);
 }
 
+// Use test(),match(),exec() Method...
+
 function newText(){
     var s = textfield.value();
 
-    var r = /\w+@\w+\.(net|com|org)/;
-    createP(r.test(s))
+    // var r = /\w+@\w+\.(net|com|org)/;
+    // var r = /\d{3}[-.]\d{4}/g
+
+    var r = /(\d{3})[-.]\d{4}/g
+
+    // var matches = s.match(r);
+
+    // var result  = r.exec(s)
+    while(result = r.exec(s)){
+       createP(result[1]);
+        // result = r.exec(s);x
+    }   
+    /* uSING  match() */
+    // for(var i = 0;i<match.length;i++){
+        
+    //    createP(matches[i])
+    // }
+
 }
